912c9a21f578d6466012f31bf70a5f8d30041685
Jakob Buis <jakob@cubiso.nl>
2015-08-04 16:45:14 +0200

    some progress, api not working

--- api.php
+++ api.php
@@ -6,9 +6,16 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+// Start Slim
+require 'vendor/autoload.php';
+$app = new \Slim\Slim();
+
 // JSON-encoded data of all current members with passes
 $app->get('/users', function(){
-
+    return json_encode([
+        ['uid' => 'jakob', 'name' => 'Jakob Buis', 'pass' => true, 'access' => true],
+        ['uid' => 'inge', 'name' => 'Inge de Bruin', 'pass' => true, 'access' => false]
+    ]);
 });
 
 // Grant a user access to the door
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,34 @@
 $(document).ready(function(){
-    //
+
+    /**
+     * Show an error message
+     * @param  {Error} XMLHttpRequest error object
+     * @return {undefined}
+     */
+    var showError = function(error) {
+        console.log(Error(error));
+        $('#error').html('Fatale fout bij communiceren met server.<br><br>Technische details: '+error.status+' '+error.responseURL).show();
+    };
+
+    /**
+     * Hide the error message
+     * @return {undefined}
+     */
+    var hideError = function() {
+        $('#error').html('').hide();
+    };
+
+    // Load all passes
+    $.ajax({
+        url: '/passes',
+        type: 'GET',
+        dataType: 'json',
+        success: function(passes) {
+            $(passes).each(function(pass){
+                var row = $('<tr><td>'+pass.name+'</td><td>'+pass.id+'</td></tr>');
+                row.appendTo('#passes tbody');
+            });
+        },
+        error: showError
+    });
 });
